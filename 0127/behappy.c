602613acbcfeb2a5708bd57fe2504c1ed33225ab
User <kjk020913@gmail.com>
2026-01-27 15:27:23 +0900

    주석 추가 및 코드 일부 수정

--- 0127/behappy.c
+++ 0127/behappy.c
@@ -7,8 +7,17 @@
 #include<string.h>
 int main()
 {
-    char str[10];
-    gets(str);
-    printf("%s", str);
+    char str[10];       // 9글자+널문자
+
+    // gets() 사용 (공백, 특수문자 입력 가능)
+    printf("Be happy!를 입력하세요.\n");
+    gets(str);          // 공백 포함하여 입력받음
+    puts(str);          // str 출력
+
+    // scanf() 사용 (공백 입력시 저장)
+    printf("Be happy!를 입력하세요.\n");
+    scanf("%s", str);   // 비교를 위한 scanf()사용
+    // printf("%s\n", str);  // str 출력
+    puts(str);          // str 출력
     return 0;
 }
