aa5c3e1c745ea5017b4523c06c1df5725c37fe57
User <kjk020913@gmail.com>
2026-01-27 14:59:06 +0900

    메뉴선택함수

---
+++ 0127/menu.c
@@ -0,0 +1,42 @@
+/*
+#### 문제
+다음의 main함수와 실행결과를 참고하여 함수를 추가하고 프로그램을 완성합니다.
+```c
+int main(void)
+{
+    int sel;
+
+    sel = disp_menu();
+    printf(""선택된 메뉴는 %d번 입니다. \n"", sel);
+
+    return 0;
+}
+```
+
+#### 실행결과
+1. 볶음짬뽕  
+2. 콩나물국밥  
+3. 바지락칼국수  
+
+#메뉴를 선택하세요 : 2 (Enter)
+선택된 메뉴는 2번입니다.
+*/
+#include<stdio.h>
+int disp_menu();
+int main(void)
+{
+    int sel;
+
+    sel = disp_menu();
+    printf("선택된 메뉴는 %d번 입니다. \n", sel);
+
+    return 0;
+}
+
+int disp_menu(){
+    int menu;
+    printf("1. 볶음짬뽕\n2. 콩나물국밥\n3. 바지락칼국수\n\n");
+    printf("메뉴를 선택하세요 :");
+    scanf("%d", &menu);
+    return menu;
+}
